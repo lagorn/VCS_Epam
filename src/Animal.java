8dbe0a2ac0f3d0864a29d520122c27b84ef35fce
origrim <lagorn@ro.ru>
2019-06-23 14:05:26 +0400

    add new Animals

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String do_it = "Действие";
+
+    public Animal(String do_it) {
+        this.do_it = do_it;
+    }
+
+    public String getDo_it() {
+        return do_it;
+    }
+
+    public void setDo_it(String do_it) {
+        this.do_it = do_it;
+    }
+}
