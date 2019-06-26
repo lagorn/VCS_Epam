5280ebecdeb559575af0eea7d32486044c2c6c67
origrim <lagorn@ro.ru>
2019-06-26 13:43:06 +0400

    Finished functions

--- src/ArrayProcessing.java
+++ src/ArrayProcessing.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayProcessing implements DivisionArray {
     int [] arr;
@@ -10,8 +11,12 @@ public class ArrayProcessing implements DivisionArray {
     @Override
     public int[] division_into_three(int[] arr) {
 
+        System.out.println("Массив чисел которые делятся на 3 без остатка");
+
         int [] arrnew = new int[arr.length];
         int k = 0;
+
+        System.out.println(Arrays.toString(arr));
 
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]%3==0){
@@ -19,18 +24,62 @@ public class ArrayProcessing implements DivisionArray {
                 k++;
             }
         }
-        
+        int [] array_modified = Arrays.copyOfRange(arrnew,0,k);
 
-        return new int[0];
+        System.out.println(Arrays.toString(array_modified));
+
+        System.out.println("---------------------------------------------------------");
+
+        return array_modified;
     }
 
     @Override
     public int[] division_into_seven(int[] arr) {
-        return new int[0];
+
+        System.out.println("Массив чисел которые делятся на 7 без остатка");
+
+        int [] arrnew = new int[arr.length];
+        int k = 0;
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%7==0){
+                arrnew[k] = arr[i];
+                k++;
+            }
+        }
+        int [] array_modified = Arrays.copyOfRange(arrnew,0,k);
+
+        System.out.println(Arrays.toString(array_modified));
+
+        System.out.println("---------------------------------------------------------");
+
+        return array_modified;
     }
 
     @Override
     public int[] division_into_twenty_one(int[] arr) {
-        return new int[0];
+
+        System.out.println("Массив чисел которые делятся на 21 без остатка");
+
+        int [] arrnew = new int[arr.length];
+        int k = 0;
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%21==0){
+                arrnew[k] = arr[i];
+                k++;
+            }
+        }
+        int [] array_modified = Arrays.copyOfRange(arrnew,0,k);
+
+        System.out.println(Arrays.toString(array_modified));
+
+        System.out.println("---------------------------------------------------------");
+
+        return array_modified;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         int [] arrai = {2,3,77,5,8,9,6,12,21,58,64,73,81,92,112,120,137};
         ArrayProcessing array_new = new ArrayProcessing(arrai);
         array_new.division_into_three(arrai);
+        array_new.division_into_seven(arrai);
+        array_new.division_into_twenty_one(arrai);
     }
 }
